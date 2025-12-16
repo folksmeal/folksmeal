d63c9d1abcbee4a89d27f27294422d430ce03041
Sam8239 <samanta.shubham99@gmail.com>
2025-12-16 19:07:25 +0530

    chore: Remove a file.

--- constants/trust.ts
+++
@@ -1,6 +0,0 @@
-export const trustStats = [
-    { label: "Corporate Clients", value: "50+" },
-    { label: "Daily Meals", value: "2,000+" },
-    { label: "Retention Rate", value: "98%" },
-    { label: "Cities", value: "Delhi NCR" },
-];
